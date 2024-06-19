3a130a7d24c9e961eb2d3dd146663f2d9b8038f6
Maria Cecilia Urrutia <currutia@buenosaires.gob.ar>
2024-06-19 13:10:06 -0300

    I initialize the project with basic installations, socket.io config

---
+++ frontend/src/App.jsx
@@ -0,0 +1,43 @@
+import { useState } from 'react'
+import './App.css'
+import io from 'socket.io-client'
+import Chat from './Chat'
+
+
+const socket = io.connect('http://localhost:3000')
+
+
+function App() {
+  const [username, setUsername] = useState('')
+  const [room, setRoom] = useState('')
+
+  const joinRoom = () => {
+    if (!username == '' && !room == '') {
+      socket.emit("join_room", room)
+    }
+  }
+
+
+  return (
+    <>
+      <div className='chat'>
+        <h3> Join us!</h3>
+
+        <input type="text" placeholder='Type your name...'
+          onChange={e => setUsername(e.target.value)} />
+
+        <input type="text" placeholder='ID room: '
+          onChange={e => setRoom(e.target.value)} />
+
+        <button onClick={joinRoom}> Join us!</button>
+
+        <Chat socket={socket} username={username} room={room} />
+
+
+      </div>
+
+    </>
+  )
+}
+
+export default App
---
+++ frontend/src/Chat.jsx
@@ -0,0 +1,38 @@
+import React, { useState } from "react";
+
+const Chat = ({ socket, username, room }) => {
+    const [currentMessage, setCurrentMessage] = useState('')
+
+    const sendMessage = async () => {
+        if (username && currentMessage) {
+            const info = {
+                message: currentMessage,
+                room,
+                author: username,
+                time: new Date(Date.now()).getHours() + " " + new Date().getMinutes()
+
+            }
+
+            await socket.emit('send_message', info)
+        }
+    }
+
+    return (
+        <div>
+            <section className="chat-header">
+                <p>Chat on real time</p>
+            </section>
+
+            <section id="messages"></section>
+
+            <section className="chat-footer">
+                <input type="text" placeholder="Message..."
+                    onChange={e => setCurrentMessage(e.target.value)}
+                />
+                <button onClick={sendMessage}>Send &#9658;</button>
+            </section>
+        </div>
+    )
+}
+
+export default Chat;
